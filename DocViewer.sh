fa78aa0b701429752e38e84e6683351d07ff88c2
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2010-01-12 12:28:25 +0100

    Initial commit

---
+++ DocViewer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cp -r /Developer/Applications/Xcode.app /Developer/Applications/DocViewer.app
+cp -f /Applications/Dictionary.app/Contents/Resources/Dictionary.icns /Developer/Applications/DocViewer.app/Contents/Resources/appicon.icns
+
+DIR=`dirname $0`
+cp -f "$DIR/InfoPlist.strings" /Developer/Applications/DocViewer.app/Contents/Resources/English.lproj/InfoPlist.strings
